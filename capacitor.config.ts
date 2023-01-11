c67a62666122dd9466b2e7a6512da06f572b09a5
Damian Tarnawsky <damian@ionic.io>
2023-01-11 06:37:37 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.tab.change',
+  appName: 'cs-tab-change',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
